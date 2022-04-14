39fc01b491b80877dbc36e91a788a92a59e7046f
Brian Gonzales <37412568+nairdesigns@users.noreply.github.com>
2022-04-14 07:46:24 +0000

    handel click to non babel version

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,17 +1,40 @@
-// TODO: import the right dependencies
-import React from "react";
-// import ReactDOM from "react-dom";
-// TODO: code the Article component
+import React, { Component } from "react";
 
-const Article = (props) => {
-  return (
-    <div className="article">
-      <h2>{props.title}</h2>
-      <p>{props.body}</p>
-    </div>
-  );
-};
+class Article extends Component {
+  constructor(props) {
+    super(props);
 
-// TODO: export your Article component for tests
+    this.state = { clicked: false };
+
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+  // handleClick = () => {
+  //   this.setState({ clicked: !this.state.clicked });
+  // };
+
+  
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+  classNames() {
+    let classes = "article";
+    if (this.state.clicked) {
+      classes += " clicked";
+    }
+    return classes;
+  }
+
+  render() {
+    return (
+      <div classNam  e={this.classNames()} onClick={this.handleClick}>
+        <h2 className="article-title">{this.props.title}</h2>
+        <p>{this.props.body}</p>
+      </div>
+    );
+  }
+}
 
 export default Article;
