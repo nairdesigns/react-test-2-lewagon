60ced53bb4d7fe0174d02dd0c14b8ceddf6e47b5
Brian Gonzales <37412568+nairdesigns@users.noreply.github.com>
2022-04-14 07:30:23 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Article from './components/Article'
+
+function App() {
+  return (
+    <main>
+      React⚛️ + Vite⚡ + Replit🌀
+      <Article title="My fancy new product" body="Test it now!" />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Article.jsx
@@ -0,0 +1,17 @@
+// TODO: import the right dependencies
+import React from "react";
+// import ReactDOM from "react-dom";
+// TODO: code the Article component
+
+const Article = (props) => {
+  return (
+    <div className="article">
+      <h2>{props.title}</h2>
+      <p>{props.body}</p>
+    </div>
+  );
+};
+
+// TODO: export your Article component for tests
+
+export default Article;
